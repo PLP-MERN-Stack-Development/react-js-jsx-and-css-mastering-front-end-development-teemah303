da32d21ed1b5ec399131b5a659e838924bf103b6
teemah303 <amir.m1700769@st.futminna.edu.ng>
2025-11-10 12:21:49 +0100

    Add EOF marker to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+cat > src/pages/Home.jsx << 'EOF'
 import { Link } from 'react-router-dom'
 import Card from '../components/Card'
 import Button from '../components/Button'
@@ -108,3 +109,4 @@ const Home = () => {
 }
 
 export default Home
+EOF
